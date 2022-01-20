afcf7ccc56d33bfa64cd6ac729cc522babc84a9d
Archana-hr <archanatcs19@gmail.com>
2022-01-20 16:12:08 +0530

    Ame validation

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,17 +3,21 @@ package com.example.demo.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.User;
 import com.example.demo.service.IUserService;
 
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 @RestController
 public class UserController {
@@ -32,7 +36,8 @@ public class UserController {
 	
 
 	@PostMapping("/user") // create
-	void createUser(@RequestBody User user) {
+	@ResponseStatus(code = HttpStatus.CREATED)
+	void createUser(@RequestBody @Valid User user) {
 		System.out.println(user.getName());
 		userService.saveUser(user);
 	}
